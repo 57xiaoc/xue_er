8a23a0c3d8f11af16a929e405eed1ecd5994e24e
57xiaoc <2415777318@qq.com>
2021-09-22 16:52:09 +0800

    第二次更细，登录界面雏形

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'page/Tabs.dart';
 import 'page/search.dart';
 void main() {
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown
-  ]);///强制竖屏
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Tabs(),
+      home:LoginPage(),
       routes: {
         '/search':(context)=>search(),
       },
--- lib/page/Tabs.dart
+++ lib/page/Tabs.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
-import 'shou.dart';
-import 'HomePage.dart';
-import 'search.dart';
-class Tabs extends StatefulWidget {
-  Tabs({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
 
   @override
-  _TabsState createState() {
-    return _TabsState();
+  _LoginPageState createState() {
+    return _LoginPageState();
   }
 }
 
-class _TabsState extends State<Tabs> {
+class _LoginPageState extends State<LoginPage> {
   @override
   void initState() {
     super.initState();
@@ -21,78 +18,33 @@ class _TabsState extends State<Tabs> {
   void dispose() {
     super.dispose();
   }
-  int _currentIndex = 0;
-  List _page = [
-    HomePage(),
-    shou()
-  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-
     return Scaffold(
-
       appBar: AppBar(
-        title: Text('Flutter Demo'),
+        title: Text('登录页面'),
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            const SizedBox(height: 80.0,),
+            Column(
+              children: [
+                FlutterLogo(
+                  size: 100.0,
+                ),
+                const SizedBox(height:16.0),
+                Text('登录系统')
+                ,
+              ],
+            ),
+            const SizedBox(height:120.0),
 
+          ],
+        ),
       ),
-      body: _page[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (int index){
-          setState(() {
-            this._currentIndex = index;
-          });
-        },
-        items:[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('shouye')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.access_alarm),
-              title: Text('shou'))
-        ],
-      ),
-      //   items: [
-      //     BottomNavigationBarItem(icon: Icon(Icons.home),)
-      //   ],
-      // ),
     );
   }
-}
-class mybutton extends StatefulWidget {
-  mybutton({Key? key}) : super(key: key);
-
-  @override
-  _mybuttonState createState() {
-    return _mybuttonState();
-  }
-}
-
-class _mybuttonState extends State<mybutton> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Column(
-      children: [
-        Chip(label: Text('$count')),
-        RaisedButton(child:Text("按钮"),onPressed:(){
-          setState(() {
-            count++;
-          });
-        })
-      ],
-    );
-  }
-  int count  = 0;
 }
